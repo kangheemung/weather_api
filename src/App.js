6abe0d5ec6d9e8444a919cf6d815332a59bd0c11
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-29 11:12:32 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //app기능
+    //현재 위치 기반의 날씨가 보인다.
+    //2.날씨 정보에는 도시 ,섭씨 화씨 날씨상태
+    //5개의 버튼이 있다.(1개는 현재 위치 다른 도시)
+    //현재위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다.
+    //데이터를들고 오는 동안 로딩 스핀어가 돈다.
+    //useEffct에는 파라미터가 2개 들어간다.
+    /////////////////////////////////////////
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            console.log('현재위치', lat, lon);
+        });
+        console.log('getCurrentLocation');
+    };
+    useEffect(() => {
+        getCurrentLocation();
+    }, []);
+    return <div className="App">water</div>;
 }
 
 export default App;
